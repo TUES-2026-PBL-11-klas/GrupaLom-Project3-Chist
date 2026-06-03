b463e29adbd44cc5c456e0594616bd92509867d1
Lppetrov0 <demirando8@abv.bg>
2026-06-03 16:12:46 +0300

    Creating new user database

---
+++ backend/user-module/src/main/resources/db/migration/V4__create_user_rewards_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE user_rewards (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID NOT NULL,
+    reward_id UUID NOT NULL,
+    earned_at TIMESTAMP NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_reward FOREIGN KEY (reward_id) REFERENCES rewards(id),
+    CONSTRAINT uk_user_reward UNIQUE (user_id, reward_id)
+);
